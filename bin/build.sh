4f9c02e0639dfefba95bcce247c308f1b63b3c8c
userteky <userteky@gmail.com>
2024-08-18 01:48:51 -0700

    Delete bin directory

--- bin/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Install dependencies
-pip install -r requirements.txt
-
-# Build your project here
-# For example, you can run a Python script to generate static files
-python app/__init__.py
