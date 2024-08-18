4d62392e77c73ae93c3e3d8e09ec46b4b095c523
userteky <userteky@gmail.com>
2024-08-18 01:43:44 -0700

    Update and rename build_files.sh to build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Build your project here
+# For example, you can run a Python script to generate static files
+python app/__init__.py
